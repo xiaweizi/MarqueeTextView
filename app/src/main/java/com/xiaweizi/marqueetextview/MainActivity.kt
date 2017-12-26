8df0774b01fbc5bb2979dad15bcd146ca35e47f2
xiaweizi <1012126908@qq.com>
2017-12-26 14:42:19 +0800

    添加跑马灯测试用例

--- app/src/main/java/com/xiaweizi/marqueetextview/MainActivity.kt
+++ app/src/main/java/com/xiaweizi/marqueetextview/MainActivity.kt
@@ -9,6 +9,33 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        marquee.startScroll()
+        resume.setOnClickListener {
+            marquee1.resumeScroll()
+            marquee2.resumeScroll()
+            marquee3.resumeScroll()
+            marquee4.resumeScroll()
+        }
+
+        pause.setOnClickListener {
+            marquee1.pauseScroll()
+            marquee2.pauseScroll()
+            marquee3.pauseScroll()
+            marquee4.pauseScroll()
+        }
+
+        restart.setOnClickListener {
+            marquee1.startScroll()
+            marquee2.startScroll()
+            marquee3.startScroll()
+            marquee4.startScroll()
+        }
+
+        stop.setOnClickListener {
+            marquee1.stopScroll()
+            marquee2.stopScroll()
+            marquee3.stopScroll()
+            marquee4.stopScroll()
+        }
+
     }
 }
